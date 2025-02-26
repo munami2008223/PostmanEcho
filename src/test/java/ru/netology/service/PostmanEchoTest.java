8f94e1a6cceae7fbb0ea726eb5b9578a292752be
“ <munami200823@gmail.com>
2025-02-26 18:40:15 +0500

    Postman echo test

---
+++ src/test/java/ru/netology/service/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Всем привет!")
+                // Выполняемые действия
+                .when().log().all()
+                .post("/post")
+                // Проверки
+                .then().log().all()
+                .statusCode(200)
+                .body( "data", org.hamcrest.Matchers.equalTo("Всем привет!"));
+    }
+}
